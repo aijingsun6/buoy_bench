6fbd930eb9e76a95db716ad79a97a679c15dc6bc
alking <aijingsun6@gmail.com>
2019-12-30 11:46:17 +0800

    add httpc_proxy

--- src/buoy_bench.erl
+++ src/buoy_bench.erl
@@ -1,5 +1,69 @@
 -module(buoy_bench).
 -author('alking').
-
+-define(DEFAULT_TIMEOUT, 5000).
 %% API
--export([]).
+-export([
+  start/0,
+  stop/0
+]).
+
+-export([bench/5]).
+
+start() ->
+  application:start(inets).
+
+stop() ->
+  application:stop(inets).
+
+
+bench(Name, Fun, Threads, Trials, Args) ->
+  bench(Name, Fun, Threads, Trials, Args, ?DEFAULT_TIMEOUT).
+
+bench(Name, Fun, Threads, Trials, Args, Timeout) ->
+  print_result(Name, repeat_tc(Fun, Threads, Trials, Args, Timeout)).
+
+repeat_tc(Fun, Threads, Trials, Args, _Timeout) when Threads < 2 ->
+  % only one thread
+  {Time, _} = timer:tc(fun() -> repeat(1, Trials, Fun, Args) end),
+  {Time, Trials};
+repeat_tc(Fun, Threads, Trials, Args, Timeout) ->
+  PID = self(),
+  {Time, _} = timer:tc(
+    fun() ->
+      do_map(Threads, PID, Fun, Trials, Args),
+      do_reduce(Threads, Timeout * Trials)
+    end),
+  {Time, Trials * Threads}.
+
+repeat(_ThreadN, TrialsN, _Fun, _Args) when TrialsN < 1 -> ok;
+repeat(ThreadNUM, TrialsNUM, Fun, Args) ->
+  case erlang:fun_info(Fun, arity) of
+    {arity, 0} -> Fun();
+    {arity, 1} -> Fun(Args);
+    {arity, 2} -> Fun(Args, ThreadNUM);
+    {arity, 3} -> Fun(Args, ThreadNUM, TrialsNUM)
+  end,
+  repeat(ThreadNUM, TrialsNUM - 1, Fun, Args).
+
+print_result(Name, {Time, Trials}) when Time =/= 0 ->
+  io:format("~s: ~.3f us (~.2f per second)~n", [Name, Time / Trials, Trials / (Time / 1000000)]);
+print_result(_, _) ->
+  ok.
+
+do_map(0, _PID, _F, _Trials, _Args) -> ok;
+do_map(N, PID, F, Trials, Args) ->
+  erlang:spawn(
+    fun() ->
+      R = repeat(N, Trials, F, Args),
+      PID ! R
+    end),
+  do_map(N - 1, PID, F, Trials, Args).
+
+do_reduce(0, _Timeout) -> ok;
+do_reduce(N, Timeout) ->
+  receive
+    _ ->
+      do_reduce(N - 1, Timeout)
+  after Timeout ->
+    do_reduce(N - 1, Timeout)
+  end.
--- src/httpc_proxy.erl
+++ src/httpc_proxy.erl
@@ -4,7 +4,11 @@
 -behaviour(gen_server).
 
 %% API
--export([start_link/0]).
+-export([
+  start_link/1,
+  request/1,
+  request/4
+]).
 
 %% gen_server callbacks
 -export([
@@ -17,13 +21,28 @@
 ]).
 
 -define(SERVER, ?MODULE).
+-define(PROFILE_ETS, httpc_proxy_profile_ets).
+-define(PROFILE_COUNTER_ETS, httpc_proxy_profile_counter_ets).
+-define(PROFILE_COUNTER_KEY, 1).
 
--record(state, {}).
-start_link() ->
-  gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+-record(state, {size}).
 
-init([]) ->
-  {ok, #state{}}.
+start_link(Size) ->
+  gen_server:start_link({local, ?SERVER}, ?MODULE, [Size], []).
+
+request(URL) ->
+  request(get, {URL, []}, [], []).
+
+request(Method, Request, HTTPOptions, Options) when is_atom(Method) ->
+  Profile = select_profile(),
+  httpc:request(Method, Request, HTTPOptions, Options, Profile).
+
+init([Size]) ->
+  ets:new(?PROFILE_ETS, [public, named_table, set, {read_concurrency, true}]),
+  ets:new(?PROFILE_COUNTER_ETS, [public, named_table, set, {read_concurrency, true}]),
+  ets:insert(?PROFILE_COUNTER_ETS, {?PROFILE_COUNTER_KEY, 1, Size}),
+  lists:foreach(fun(X) -> add_profile(X) end, lists:seq(1, Size)),
+  {ok, #state{size = Size}}.
 
 handle_call(_Request, _From, State) ->
   {reply, ok, State}.
@@ -43,3 +62,16 @@ code_change(_OldVsn, State, _Extra) ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+profile_name(IDX) ->
+  erlang:list_to_atom(lists:concat(["profile_", IDX])).
+
+add_profile(IDX) ->
+  Name = profile_name(IDX),
+  {ok, PID} = httpc_profile_sup:start_child([{profile, Name}]),
+  ets:insert(?PROFILE_ETS, {IDX, Name, PID}).
+
+select_profile() ->
+  [{_, _, Max}] = ets:lookup(?PROFILE_COUNTER_ETS, ?PROFILE_COUNTER_KEY),
+  IDX = ets:update_counter(?PROFILE_COUNTER_ETS, ?PROFILE_COUNTER_KEY, {2, 1, Max, 1}),
+  [{_, Name, _}] = ets:lookup(?PROFILE_ETS, IDX),
+  Name.
